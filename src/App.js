8ada1eb8b9593a8286151878a21a624e05650028
Kyle <wilichowskikyle@gmail.com>
2020-04-20 12:04:21 -0500

    fix react app

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <head>
-        <meta charset="utf-8" />
-        <title></title>
-        <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
-        <script src="https://assets.shiftr.io/js/mqtt-2.9.0.js"></script>
-        <script src="script.js" charset="utf-8"></script>
-      </head>
-      <body>
-        <button id="button">Click Me</button>
-      </body>
+      <button id="button">Click Me</button>
     </div>
   );
 }
